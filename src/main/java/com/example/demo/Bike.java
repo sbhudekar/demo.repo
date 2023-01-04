3a0d3ddd0ec540516db6ebfd3f2a900c32f0c754
sbhud <sbhudekar1896@gmail.com>
2023-01-04 23:51:57 +0530

    commited bike

---
+++ src/main/java/com/example/demo/Bike.java
@@ -0,0 +1,9 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+public class Bike {  @GetMapping("/myflight")
+public String gettflightData(){
+    return "Please book your hhhh Train ticket, Choose Your Fav Express";
+}
+}
